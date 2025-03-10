aabe6482e72f17991bbc337befaf834952afa3c5
ManHinnn0509 <79153615+ManHinnn0509@users.noreply.github.com>
2025-03-10 09:10:25 +0800

    Fix LARGE model filename

--- gradio/config.py
+++ gradio/config.py
@@ -7,6 +7,8 @@ class Pth_Profile:
         self.hidden_size = hidden_size
         self.patch_len = patch_len
 
+# SMALL and MEDIUM doesn't work? It stucks when generating
+
 SMALL = Pth_Profile(
     'weights_notagen_pretrain_p_size_16_p_length_2048_p_layers_12_c_layers_3_h_size_768_lr_0.0002_batch_8.pth',
     12, 3, 768, 2048
@@ -18,7 +20,7 @@ MEDIUM = Pth_Profile(
 )
 
 LARGE = Pth_Profile(
-    'weights_notagen_pretrain_p_size_16_p_length_1024_p_layers_20_c_layers_6_h_size_1280_lr_0.0001_batch_4.pth',
+    'weights_notagenx_p_size_16_p_length_1024_p_layers_20_h_size_1280.pth',
     20, 6, 1280, 1024
 )
 
